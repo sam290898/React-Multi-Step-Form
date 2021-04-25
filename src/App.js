403f7dd803549f97a57f2e7594fce9806dcdac8b
Salman Khan <62479262+sam290898@users.noreply.github.com>
2021-04-25 17:45:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import {UserForm} from './Components/UserForm';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <UserForm /> 
+      </div>
+    );
+  }
+}
+
+export default App;
